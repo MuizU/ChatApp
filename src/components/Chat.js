39e1fec965ec51dc9fb0b8d652097ad7215793b8
MuizU <w1673601@my.westminster.ac.uk>
2018-05-06 14:53:39 +0530

    Origin.

---
+++ src/components/Chat.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import{
+  View,
+  Text
+}from 'react-native';
+class Chat extends React.Component{
+  render(){
+    return(
+    <View>
+    <Text>
+    Hello Chat
+    </Text>
+    </View>
+  );
+  }
+}
+export default Chat;
