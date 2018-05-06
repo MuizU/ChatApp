c6001b13f0d218435d52deca94d8f78e27461787
MuizU <w1673601@my.westminster.ac.uk>
2018-05-06 23:43:46 +0530

    Created home page with functionality.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,56 @@
-import react from 'react';
+import React from 'react';
 import{
   View,
-  Text
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
 }from 'react-native';
 class Home extends React.Component{
+  state = {
+    name:'',
+  }
   render(){
     return(
     <View>
-    <Text>
-    Hello Home
+    <Text style={styles.title}>
+    Enter your name:
     </Text>
+    <TextInput
+      style={styles.nameInput}
+      placeholder='Muiz Uvais'
+      onChangeText={(text) => {
+        this.setState({
+          name:text,
+        });
+      }
+      }
+    />
+    <TouchableOpacity
+    onPress>
+    <Text style={styles.button}>
+      Next
+    </Text>
+    </TouchableOpacity>
     </View>
   );
   }
 }
+var styles = StyleSheet.create({
+  title:{
+    marginTop:20,
+    marginLeft:20,
+    fontSize:20,
+  },
+  nameInput: {
+    height:40,
+    borderWidth:2,
+    borderColor:'black',
+    margin:20,
+  },
+  button:{
+    marginLeft:20,
+    fontSize:20,
+  }
+})
 export default Home;
